d114d614defee40d96f5cfc90a718c526744f75d
sk814 <sanjeev.cs.singh@gmail.com>
2020-06-24 20:46:53 +0100

    actvity 4 firebase

--- app/src/main/java/com/example/echovectorapp2/Activity3.java
+++ app/src/main/java/com/example/echovectorapp2/Activity3.java
@@ -1,14 +1,54 @@
 package com.example.echovectorapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Activity3 extends AppCompatActivity {
+
+    private static final String TAG = "Activity3";
+    private TextView product;
+    private TextView description;
+
+    //Connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        //Getting by ids
+        description = findViewById(R.id.productDescription);
+        product = findViewById(R.id.productTitle);
+
+        db.collection("products_uploaded")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/echovectorapp2/Activity4.java
+++ app/src/main/java/com/example/echovectorapp2/Activity4.java
@@ -1,14 +1,88 @@
 package com.example.echovectorapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Activity4 extends AppCompatActivity {
+
+    private static final String TAG = "Activity4";
+    private EditText product;
+    private EditText description;
+    private Button uploadButton;
+
+    //Keys
+    public static final String KEY_PRODUCT = "product";
+    public static final String KEY_DESCRIPTION = "description";
+
+    //Connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+
+        //Getting by ids
+        uploadButton = findViewById(R.id.uploadButton);
+        product = findViewById(R.id.productNameUpload);
+        description = findViewById(R.id.productDescriptionUpload);
+
+        // Create a new user with a name, email, and password
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String productField = product.getText().toString().trim();
+                String descriptionField = description.getText().toString().trim();
+
+                Map<String, Object> products = new HashMap<>();
+
+
+                products.put(KEY_PRODUCT, productField);
+                products.put(KEY_DESCRIPTION, descriptionField);
+
+
+                if(!TextUtils.isEmpty(productField)) {
+                    db.collection("Products_Posted")
+                            .add(products)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Toast.makeText(Activity4.this, "Successfully Uploaded!", Toast.LENGTH_LONG).show();
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Activity4.this, "Error! Upload failed.", Toast.LENGTH_LONG).show();
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+
+                            });
+                }else{
+                    Toast.makeText(Activity4.this, "Please enter the product name.", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
     }
 }
